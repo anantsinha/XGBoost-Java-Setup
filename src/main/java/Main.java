03e9d93d5f0eb706b59e33541b385878c33bd8e8
Anant Sinha <anant.sinha@qohash.com>
2022-08-20 23:54:23 -0500

    Import Appropriate XGBoost libraries

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import ml.dmlc.xgboost4j.java.DMatrix;
+import ml.dmlc.xgboost4j.java.XGBoostError;
+import ml.dmlc.xgboost4j.java.Booster;
+import ml.dmlc.xgboost4j.java.XGBoost;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Gralde Base Project executies successfully");
