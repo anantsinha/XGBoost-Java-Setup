7e63d6c4a6f7addf0a253797d34fdba4e21380d5
Anant Sinha <anant.sinha@qohash.com>
2022-08-21 00:01:32 -0500

    Add code to load model in Java, load test data in libsvm format and run predictions

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,13 @@ import ml.dmlc.xgboost4j.java.Booster;
 import ml.dmlc.xgboost4j.java.XGBoost;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Gralde Base Project executies successfully");
+    public static void main(String[] args) throws XGBoostError {
+        Booster booster = XGBoost.loadModel("xgbModel.model");
+        DMatrix dtest = new DMatrix("libsvmData.txt");
+        System.out.println("Loaded data");
+        float[][] predicts = booster.predict(dtest);
+        // nth predicted value is extracted by predicts[n][0]
+        System.out.println("First predicted value is:");
+        System.out.println(predicts[0][0]);
     }
 }
